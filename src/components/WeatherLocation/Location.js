12563d043cc1e84cd23ddd3232952dd4de4fa428
Nestor Garcia <nestorgs77@gmail.com>
2019-10-14 19:08:55 -0500

    Adding style to WeatherLocation and Location Comp

--- src/components/WeatherLocation/Location.js
+++ src/components/WeatherLocation/Location.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types' ;
-
+import './style.css';
 const Location = ({city}) => (
 <div className = "locationCont"><h1>{city}</h1></div>
 
--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -2,6 +2,7 @@
 import React from 'react'; 
 import WeatherData from './WeatherData';
 import Location from './Location';
+import './style.css';
 //import PropTypes from 'prop-types';
 //import WeatherExtraInfo from './WeatherData/WeatherExtraInfo';
 //import WeatherTemperature from './WeatherData/WeatherTemperature';
