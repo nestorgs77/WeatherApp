0fdc5c3d2ea6391e3721a830969197721a0d03cd
Nestor Garcia <nestorgs77@gmail.com>
2019-10-16 16:32:21 -0500

    Creating a class component and using the render method ,Validanting objects with the shape  proptype

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -1,5 +1,6 @@
 //Importando desde la libreria React
-import React from 'react'; 
+import React , {Component}  from 'react'; 
+import PropTypes from 'prop-types';
 import WeatherExtraInfo from './WeatherData/WeatherExtraInfo';
 import WeatherTemperature from './WeatherData/WeatherTemperature';
 import WeatherData from './WeatherData';
@@ -25,15 +26,37 @@ wind : 25,
 
 //import PropTypes from 'prop-types';
 
-//Creando el componente llamado WeatherLocation
-const WeatherLocations =  () => (
-<div className="weatherLocationCont" >
-     <Location city={"Barcelona"}></Location>
-     <WeatherData data={data}></WeatherData>
+//Creating a kind component called class component and rendering !!!
+//In React there are two kind of component , 1- Class Component and 2- Functional Component
+class WeatherLocations extends Component {
+render () {
+  return (<div className="weatherLocationCont" >
+  <Location city={"Barcelona"}></Location>
+  <WeatherData data={data}></WeatherData>
+ 
+</div>);
+
+}
+
+}
+
     
-</div>
-    
-);
+
+//Validating Object date created before using the Proptype shape
+WeatherData.propType = {
+    data : PropTypes.shape({
+    temperature:PropTypes.number.isRequired,
+    weatherState:PropTypes.string.isRequired,
+    humidity:PropTypes.number.isRequired,
+    wind:PropTypes.number.isRequired,
+
+
+
+    })
+
+
+
+}
 
 //Esta linea de abajo indica que puedes invocar este componente desde cualquier parte de la aplicacion 
 export default WeatherLocations ; 
