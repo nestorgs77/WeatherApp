04339e6dd8591c8bdd1909681245c33bf784054a
Nestor Garcia <nestorgs77@gmail.com>
2019-10-18 18:21:57 -0500

    Consuming  Openweathermap API

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -17,23 +17,57 @@ import {
 
   } from '../../constants/weathers'
 
+   const location = "Buenos Aires,ar";
+   const api_key = "fc0e39618f16ec2d7d35e090c5989368";
+   const url_base_weather = "api.openweathermap.org/data/2.5/forecast";
+
+   const api_weather = `${url_base_weather}?q=${location}&APPID=${api_key}`;
+
 const data ={
  temperature: 5,
  weatherState: SUN, 
 humidity :10 , 
 wind : 25,
 }
+const data2 ={
+  temperature: 10,
+  weatherState: WINDY, 
+ humidity :30 , 
+ wind : 50,
+ }
 
 //import PropTypes from 'prop-types';
 
 //Creating a kind component called class component and rendering !!!
 //In React there are two kind of component , 1- Class Component and 2- Functional Component
 class WeatherLocations extends Component {
+    
+  constructor (){
+  super ();
+  this.state = {
+     city : "Buenos Aires" , 
+     data : data ,
+}
+
+  
+
+  }
+  handleUpdate =()=> {
+    fetch(api_weather);
+    console.log("Actualizado ");
+    this.setState({
+      city:"Madrid" , 
+      data:data2 ,
+    })
+  }
+
 render () {
+//Using Destructuring
+const {city , data}= this.state;
   return (<div className="weatherLocationCont" >
-  <Location city={"Barcelona"}></Location>
+  <Location city={city}></Location>
   <WeatherData data={data}></WeatherData>
- 
+ <button onClick = {this.handleUpdate}>Actualizar</button>
 </div>);
 
 }
